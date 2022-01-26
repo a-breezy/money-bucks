d8924c96c7ebd76fca9f1f756c0aef4d70c3a8af
a.breezy <ambrose.wilkins@gmail.com>
2022-01-26 12:08:26 -0500

    update product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -62,14 +62,19 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-	Product.create({
+	// Product.create({
+	// 	product_name: req.body.product_name,
+	// 	price: req.body.price,
+	// 	stock: req.body.stock,
+	// 	tagIds: req.body.tagIds /* [1, 2, 3, 4] */,
+	// });
+
+	Product.create(req.body, {
 		product_name: req.body.product_name,
 		price: req.body.price,
 		stock: req.body.stock,
-		tagIds: req.body.tagIds /* [1, 2, 3, 4] */,
-	});
-
-	Product.create(req.body)
+		tagIds: [req.body.tagIds],
+	})
 		.then((product) => {
 			// if there's product tags, we need to create pairings to bulk create in the ProductTag model
 			if (req.body.tagIds.length) {
@@ -133,8 +138,24 @@ router.put("/:id", (req, res) => {
 		});
 });
 
+// delete one product by its `id` value
 router.delete("/:id", (req, res) => {
-	// delete one product by its `id` value
+	Product.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbProductData) => {
+			if (!dbProductData) {
+				res.status(404).json({ message: "No product with this id" });
+				return;
+			}
+			res.json(dbProductData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
