c0de7f9752f503e95faf3694778f6197680c0b13
a.breezy <ambrose.wilkins@gmail.com>
2022-01-26 19:16:43 -0500

    completed product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -62,19 +62,15 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-	// Product.create({
-	// 	product_name: req.body.product_name,
-	// 	price: req.body.price,
-	// 	stock: req.body.stock,
-	// 	tagIds: req.body.tagIds /* [1, 2, 3, 4] */,
-	// });
-
-	Product.create(req.body, {
-		product_name: req.body.product_name,
-		price: req.body.price,
-		stock: req.body.stock,
-		tagIds: [req.body.tagIds],
-	})
+	/* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
+	Product.create(req.body)
 		.then((product) => {
 			// if there's product tags, we need to create pairings to bulk create in the ProductTag model
 			if (req.body.tagIds.length) {
@@ -98,7 +94,6 @@ router.post("/", (req, res) => {
 
 // update product
 router.put("/:id", (req, res) => {
-	// update product data
 	Product.update(req.body, {
 		where: {
 			id: req.params.id,
